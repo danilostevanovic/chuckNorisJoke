e583e7288d431e5aaa61386f601ceb808989b03f
djokacalije <djokacalije@gmail.com>
2018-04-22 19:29:36 +0200

    number

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ document.querySelector('.get-jokes').addEventListener('click', getJokes);
 function getJokes(e) {
   const xhr =new XMLHttpRequest();
   const number = document.getElementById("number").value;
-  
+  if(number.value<0){
+    number.value=0;
+  }
 
   xhr.open('GET',`https://api.icndb.com/jokes/random/${number}`,true);
   
